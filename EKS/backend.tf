8db39aab9889eb0ffa2650148f9a0c68f24e3630
Al-amin Otusanya <al-aminotusanya@Al-amins-MacBook-Air.local>
2024-04-07 17:31:22 -0400

    updating iam user and attching to eks

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    profile = "jenkins-eks-tf"
     bucket = "my-project-backend"
     key    = "eks/terraform.tfstate"
     region = "us-west-1"
--- EKS/provider.tf
+++ EKS/provider.tf
@@ -9,6 +9,5 @@ terraform {
 
 provider "aws" {
   region = "us-west-1"
-  profile = "jenkins-eks-tf"
 
 }
